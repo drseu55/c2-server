7c536198f37815181fdfcbfd277c6307d25bef7d
drseu55 <alpics1337@gmail.com>
2022-05-17 16:18:53 +0300

    feat: Convert errors to custom error type

--- src/errors.rs
+++ src/errors.rs
@@ -1,3 +1,5 @@
+use actix_web::HttpResponse;
+use actix_web::ResponseError;
 use thiserror::Error;
 
 #[derive(Error, Debug)]
@@ -13,5 +15,36 @@ pub enum ServerError {
 impl std::convert::From<argon2::password_hash::Error> for ServerError {
     fn from(err: argon2::password_hash::Error) -> Self {
         ServerError::InternalServerError
+    }
+}
+
+impl std::convert::From<r2d2::Error> for ServerError {
+    fn from(err: r2d2::Error) -> Self {
+        ServerError::InternalServerError
+    }
+}
+
+impl std::convert::From<actix_web::Error> for ServerError {
+    fn from(err: actix_web::Error) -> Self {
+        ServerError::InternalServerError
+    }
+}
+
+impl std::convert::From<diesel::result::Error> for ServerError {
+    fn from(err: diesel::result::Error) -> Self {
+        ServerError::InternalServerError
+    }
+}
+
+// impl ResponseError trait allows to convert our errors into http responses with appropriate data
+impl ResponseError for ServerError {
+    fn error_response(&self) -> HttpResponse {
+        match self {
+            ServerError::InternalServerError => {
+                HttpResponse::InternalServerError().json("Internal Server Error, Please try later")
+            }
+            ServerError::BadRequest(ref message) => HttpResponse::BadRequest().json(message),
+            ServerError::Unauthorized => HttpResponse::Unauthorized().json("Unauthorized"),
+        }
     }
 }
